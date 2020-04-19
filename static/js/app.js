9e1f3701ba6539cca73316a9e31d962779b5f075
Rob <robcreel@gmail.com>
2020-04-19 17:00:24 -0500

    Light refactoring.

--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,31 @@
 // from data.js
 var tableData = data;
 
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
 // Select the button
-var button = d3.select("#button");
+var button = d3.select("#filter-btn");
+
+
+// Function to list reports 
+var listReports = (reports) => {
+  tbody.html("")
+  reports.forEach((report) => {
+    var row = tbody.append("tr");
+    Object.entries(report).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  })
+};
+
 
 // console.log(tableData.filter(report => report.datetime === "1/1/2011"))
-console.log("hi")
-button.on("click", function() {
-    console.log("hi")
 
-  // Select the input element and get the raw HTML node
+
+button.on("click", () => {
+    // Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
 
   // Get the value property of the input element
@@ -22,29 +38,16 @@ button.on("click", function() {
 
   console.log(filteredData);
 
-});
+  if (filteredData.length === 0) {
+    listReports(tableData)
+  } else {
+    listReports(filteredData)
+  }
+  // listReports(filteredData.length ?  filteredData : tableData)
 
-
-// // // Send data to HTML via D3.
-// // data.forEach((ufoSightingReport) => {
-// //   var row = tbody.append("tr");
-// //   Object.entries(ufoSightingReport).forEach(([key, value]) => {
-// //     var cell = row.append("td");
-// //     cell.text(value);
-// //   });
-// // });
-
-// Get a reference to the table body
-var tbody = d3.select("tbody");
+})
 
 // Send data to HTML via D3.
-tableData.forEach((ufoSightingReport) => {
-  var row = tbody.append("tr");
-  Object.entries(ufoSightingReport).forEach(([key, value]) => {
-    var cell = row.append("td");
-    cell.text(value);
-  });
-});
-
+listReports(tableData)
 
 
